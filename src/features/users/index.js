62dca3bb640f4563078ea5e9f7beeb5ed82d6026
jppolancop <polancopuerta28@gmail.com>
2026-07-27 08:13:43 -0500

    todo lo que llevamos con edwar hasta el momento

---
+++ src/features/users/index.js
@@ -0,0 +1,3 @@
+﻿export { default as UserRegisterForm } from "./components/UserRegisterForm";
+export { default as UserListPage } from "./pages/UserListPage";
+export { default as ReportConfigModal } from "./reports/components/ReportConfigModal";
